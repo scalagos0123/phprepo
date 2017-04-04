06c41c04148edffd58ca40258acc8408d328315e
Shaun <scalagos0123@live.com>
2017-04-04 20:17:00 +0800

    printing all users

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
-<?php
+<?php 
 
-echo "Hello World";
+include ("config/connect.php");
+
+function getUsers() {
+    $result = $connection->query("select * from users");
+    return $result->fetch_row(MYSQLI_ASSOC);
+}
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach (getUsers() as $users) { ?>
+                <tr>
+                    <td><?=$users["user_id"]?></td>
+                    <td><?=$users["user_name"]?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </body>
+</html>
